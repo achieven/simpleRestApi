2a35d25f16b91085e5205799b2da52fdeeeaae12
achieven <achieven@users.noreply.github.com>
2016-09-22 16:52:00 +0300

    using process.env.port

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ app.delete('/profiles/:profileId', function(req,res){
         res.status(200).send(response)
     })
 })
-
-app.listen(3000, function () {
-    console.log('listening on port 3000')
+const port = process.env.PORT || 3000
+app.listen(port, function () {
+    console.log('listening on port ' + port)
 })
